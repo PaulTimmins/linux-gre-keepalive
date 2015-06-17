f2bc1e5570cc148da7dc2f5cd4b6af9c27d61528
Paul Timmins <paul@timmins.net>
2015-06-17 12:23:45 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # linux-gre-keepalive
 Userspace daemon in perl to handle Cisco GRE keepalives. Works in Linux, should work in any *nix derivative
+
+Requires Net::Pcap, NetPacket::IP, and Proc::Daemon
+
+(all 3 have stable debian perl packages in the standard repositories)
+
+Usage:
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+ip tunnel add mytunnel mode gre remote x.x.x.x local y.y.y.y ttl 255 pmtudisc
+ip link set mytunnel up
+./gre-keepalive.pl mytunnel
+
+
+This daemon does not initiate keepalive packets, but does look for ones sent by the originating system and redirects them as a standard Cisco router would, thus causing the GRE tunnel to go up/up, and causing it to go up/down if connectivity is lost. 
---
+++ gre-keepalive.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use Net::Pcap;
+use NetPacket::IP;
+use FileHandle;
+use Socket;
+use constant DUMMY_ADDR  => scalar(sockaddr_in(0, inet_aton('1.0.0.0')));
+
+use Proc::Daemon;
+
+Proc::Daemon::Init;
+
+my $continue = 1;
+#$SIG{TERM} = sub { $continue = 0 };
+
+    socket(RAW, AF_INET, SOCK_RAW, 255) || die $!;
+    setsockopt(RAW, 0, 1, 1);
+    my $dev = $ARGV[0];
+    # open the device for live listening
+    my $pcap = Net::Pcap::open_live($dev, 1024, 0, 0, \$err);
+    $filter="proto gre";
+    if ( Net::Pcap::compile($pcap, \$filter_t, $filter, $opt, $net) == -1 ) {
+        die "Unable to compile filter string '$filter'\n";
+    }
+
+    Net::Pcap::setfilter($pcap,$filter_t);
+    Net::Pcap::loop($pcap, -1 , \&process_packet, "just for the demo");
+
+    # close the device
+    Net::Pcap::close($pcap);
+
+    sub process_packet {
+        my ($user_data, $header, $packet) = @_;
+	my $packet = unpack("x16a*",$packet);
+        my $pkt = NetPacket::IP->decode($packet);
+	print "Sending $packet to $pkt->{'dest_ip'}\n";
+	send(RAW,$packet,0,DUMMY_ADDR) or die "Couldn't send packet: $!";	
+        print "Sent to $pkt->{'dest_ip'}\n"; 
+	# do something ...
+    }
+
