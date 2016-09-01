a8928d04d2496b34dbdcb27a999ada7ede87a3d8
Richard Laager <rlaager@wiktel.com>
2016-09-01 06:38:50 -0500

    Use strict and warnings

--- gre-keepalive.pl
+++ gre-keepalive.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/perl
+
+use strict;
+use warnings;
+
 use Net::Pcap;
 use NetPacket::IP;
 use Proc::Daemon;
@@ -11,10 +15,12 @@ socket(RAW, AF_INET, SOCK_RAW, 255) || die $!;
 setsockopt(RAW, 0, 1, 1);
 
 my $dev = $ARGV[0];
+my $err;
 my $pcap = Net::Pcap::open_live($dev, 1024, 0, 0, \$err);
 
-$filter = "proto gre";
-if (Net::Pcap::compile($pcap, \$filter_t, $filter, $opt, $net) == -1) {
+my $filter = "proto gre";
+my $filter_t;
+if (Net::Pcap::compile($pcap, \$filter_t, $filter, 0, 0) == -1) {
     die "Unable to compile filter string '$filter'\n";
 }
 Net::Pcap::setfilter($pcap, $filter_t);
@@ -25,7 +31,7 @@ Net::Pcap::close($pcap);
 
 sub process_packet {
     my ($user_data, $header, $packet) = @_;
-    my $packet = unpack("x16a*", $packet);
+    $packet = unpack("x16a*", $packet);
     my $pkt = NetPacket::IP->decode($packet);
     print "Sending $packet to $pkt->{'dest_ip'}\n";
     send(RAW, $packet, 0, DUMMY_ADDR) or die "Couldn't send packet: $!";
