1e53c4160453385b3c3c53f85c39443276e3b8aa
Richard Laager <rlaager@wiktel.com>
2016-09-01 06:02:22 -0500

    Space consistently

--- gre-keepalive.pl
+++ gre-keepalive.pl
@@ -17,22 +17,22 @@ setsockopt(RAW, 0, 1, 1);
 my $dev = $ARGV[0];
 
 my $pcap = Net::Pcap::open_live($dev, 1024, 0, 0, \$err);
-$filter="proto gre";
-if ( Net::Pcap::compile($pcap, \$filter_t, $filter, $opt, $net) == -1 ) {
+$filter = "proto gre";
+if (Net::Pcap::compile($pcap, \$filter_t, $filter, $opt, $net) == -1) {
     die "Unable to compile filter string '$filter'\n";
 }
 
-Net::Pcap::setfilter($pcap,$filter_t);
-Net::Pcap::loop($pcap, -1 , \&process_packet, "just for the demo");
+Net::Pcap::setfilter($pcap, $filter_t);
+Net::Pcap::loop($pcap, -1, \&process_packet, "just for the demo");
 
 Net::Pcap::close($pcap);
 
 sub process_packet {
     my ($user_data, $header, $packet) = @_;
-    my $packet = unpack("x16a*",$packet);
+    my $packet = unpack("x16a*", $packet);
     my $pkt = NetPacket::IP->decode($packet);
     print "Sending $packet to $pkt->{'dest_ip'}\n";
-    send(RAW,$packet,0,DUMMY_ADDR) or die "Couldn't send packet: $!";
+    send(RAW, $packet, 0, DUMMY_ADDR) or die "Couldn't send packet: $!";
     print "Sent to $pkt->{'dest_ip'}\n";
 }
 
